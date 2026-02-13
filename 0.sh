8fa6e11dd3c493b6cdda3cb4b7f67abff13e7b13
vulcannodes97-sudo <vulcannodes97@gmail.com>
2026-02-13 14:13:56 +0530

    Create 0.sh

---
+++ 0.sh
@@ -0,0 +1,7 @@
+echo "# ttest" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/vulcannodes97-sudo/ttest.git
+git push -u origin main
